5c7cbc9765aaf2c0d438a8c121fc12aebd7d3335
twodayslate <zac@gorak.us>
2020-04-22 18:49:47 -0400

    Added niNameOrNil initializer

--- Sources/SplitTabBar/SplitTabBarMasterNavigationController.swift
+++ Sources/SplitTabBar/SplitTabBarMasterNavigationController.swift
@@ -3,6 +3,11 @@ import UIKit
 open class SplitTabBarMasterNavigationController: UINavigationController {
     public let navigation: SplitTabBarMasterController
 
+    override public init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        self.navigation = SplitTabBarMasterController()
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
     public init(navigation: SplitTabBarMasterController) {
         self.navigation = navigation
         super.init(rootViewController: self.navigation)
